4c9a164f14cbdca82622d205b3be49c49b3a7b8a
saito-y <saito-y@applicative.co.jp>
2014-10-21 16:16:17 +0900

    Chap15終了

--- src/Chap15/Circle.java
+++ src/Chap15/Circle.java
@@ -1,8 +1,10 @@
 package Chap15;
 
 public class Circle {
+	
 	public static void main(String[] args){
 		double r = Double.parseDouble(args[0]);
-		System.out.printf("”¼Œa%.2f‚Ì‰~ü‚Í%.2f‚Å–ÊÏ‚Í%.2f‚Å‚·B\n", r, 2 * Math.PI * r * r);
+		System.out.printf("”¼Œa%.2f‚Ì‰~ü‚Í%.2f‚Å–ÊÏ‚Í%.2f‚Å‚·B\n",
+				r, 2 * Math.PI * r, Math.PI * r * r);
 	}
 }
